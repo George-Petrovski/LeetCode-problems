ba82014203f82d60e958a4a823f05c913fde8b15
George Petrovski <georcje@hotmail.com>
2023-04-24 22:54:27 -0400

    Solution for two sum

---
+++ two_sum.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, value in enumerate(nums):
+            remaining = target - nums[i]
+
+            if remaining in seen:
+                return [seen[remaining], i]
+            
+            seen[value] = i
