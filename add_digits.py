aedd4b6d37db934838903471a376a1d080e2f880
George Petrovski <georcje@hotmail.com>
2023-04-25 23:08:48 -0400

    Solution for add digits

--- add_digits.py
+++ add_digits.py
@@ -1,8 +1,15 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        while (num > 9):
-            sep_num = [int(d) for d in str(num)]
-            num = 0
-            for i in range(len(sep_num)):
-                num += sep_num[i]
+        # while (num > 9):
+        #     sep_num = [int(d) for d in str(num)]
+        #     num = 0
+        #     for i in range(len(sep_num)):
+        #         num += sep_num[i]
+        # return num
+        while num > 9:
+            sum = 0
+            while num:
+                sum += num%10
+                num = num//10
+            num = sum
         return num
