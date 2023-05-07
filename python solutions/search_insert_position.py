519812c1cf832d464e0577c7a28774aa69318e34
George Petrovski <georcje@hotmail.com>
2023-05-06 23:54:29 -0400

    Solution for search insert position

---
+++ python solutions/search_insert_position.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)
+
+        while l < r:
+            m = (l + r) // 2
+            if target > nums[m]:
+                l = m + 1
+            else:
+                r = m
+        return l
