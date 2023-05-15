5a10dff8f6b0d80d10307d410aa09949b883d060
George Petrovski <georcje@hotmail.com>
2023-05-14 20:28:18 -0400

    Mock question with Jeffrey from Google May 14 2023

---
+++ python solutions/google_mock_iv.py
@@ -0,0 +1,27 @@
+# dict {
+#     1:2, 3
+#     2:1, 3
+#     3:2, 1, 4
+#     4:3
+# }
+
+# visited_set{
+
+# }
+
+# queue ()
+
+# while !queue.empty():
+#     node = queue.pop()
+#     if visited_set.contains(node):
+#         continue
+#     visited_set.add(node)
+#     for node_to_visit in dict[node]:
+#         queue.add(node_to_visit, node.node_distance + 1)
+
+# return -1
+
+#breadth first search
+#depth first search
+
+#graph theory
