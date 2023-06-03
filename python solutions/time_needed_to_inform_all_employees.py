3e751691f157ae1592a48441c61fe3b06999fa5e
George Petrovski <georcje@hotmail.com>
2023-06-02 22:50:08 -0400

    Solution for time needed to inform all employees

---
+++ python solutions/time_needed_to_inform_all_employees.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        def find(i):
+            if manager[i] != -1:
+                informTime[i] += find(manager[i])
+                manager[i] = -1
+            return informTime[i]
+        return max(map(find, range(n)))
