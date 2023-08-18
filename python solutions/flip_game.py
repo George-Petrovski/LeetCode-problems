f19e1f6ebc3f5842f68e2331eda186ae062b19f1
George Petrovski <georcje@hotmail.com>
2023-08-17 21:47:16 -0400

    Solution for flip game

---
+++ python solutions/flip_game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def generatePossibleNextMoves(self, currentState: str) -> List[str]:
+        res = []
+        for i in range(len(currentState)-1):
+            if currentState[i:i+2] == '++':
+                res.append(currentState[:i] + '--' + currentState[i+2:])
+        return res
