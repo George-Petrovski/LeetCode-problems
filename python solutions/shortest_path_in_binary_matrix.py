0a91e9d1e1ef0257881f474c7f6c6296d8de2e26
George Petrovski <georcje@hotmail.com>
2023-09-11 13:42:20 -0400

    easier to understand solution for shortest path in binary matrix

--- python solutions/shortest_path_in_binary_matrix.py
+++ python solutions/shortest_path_in_binary_matrix.py
@@ -6,38 +6,33 @@ class Solution:
         if grid[0][0] != 0 or grid[-1][-1] != 0:
             return -1
         
-        N = len(grid)
+        max_row = len(grid) - 1
+        max_col = len(grid[0]) - 1
 
         offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-
-        q = Deque()
-        q.append((0, 0))
-        visited = {(0, 0)}
 
         def get_neighbours(x, y):
             for x_offset, y_offset in offsets:
                 new_row = x + x_offset
                 new_col = y + y_offset
                 # print(new_row, " : ", new_col)
-                if 0 <= new_row < N and 0 <= new_col < N and not grid[new_row][new_col] and (new_row, new_col) not in visited:
-                    yield (new_row, new_col)
+                if not(0 <= new_row <= max_row and 0 <= new_col <= max_col):
+                    continue
+                if grid[new_row][new_col] != 0:
+                    continue
+                yield (new_row, new_col)
 
-        current_distance = 1
+        q = Deque()
+        q.append((0, 0))
+        grid[0][0] = 1
 
         while q:
-            length = len(q)
-            print(q)
-            for _ in range(length):
-                row, col = q.popleft()
-
-                if row == N-1 and col == N-1:
-                    return current_distance
-                
-                for p in get_neighbours(row, col):
-                    print(p)
-                    visited.add(p)
-                    q.append(p)
-                
-            current_distance += 1
+            row, col = q.popleft()
+            distance = grid[row][col]
+            if (row, col) == (max_row, max_col):
+                return distance
+            for neighbour_row, neighbour_col in get_neighbours(row, col):
+                grid[neighbour_row][neighbour_col] = distance + 1
+                q.append((neighbour_row, neighbour_col))
 
         return -1
