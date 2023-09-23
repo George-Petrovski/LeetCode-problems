bcdea25cb07cab3ef98165091db603cf95a20dcf
George Petrovski <georcje@hotmail.com>
2023-09-23 18:39:31 -0400

    Solution for number of valid subarrays

---
+++ python solutions/number_of_valid_subarrays.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def validSubarrays(self, nums: List[int]) -> int:
+        stack = []
+        res = 0
+        
+        for num in nums:
+            while stack and num < stack[-1]:
+                stack.pop()
+            stack.append(num)
+            res += len(stack)
+
+        return res
