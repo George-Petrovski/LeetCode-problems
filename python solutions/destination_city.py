4cf39fdca9bcd8b25c98a415ab20eadb6ec0828b
George Petrovski <georcje@hotmail.com>
2023-12-15 16:18:34 -0500

    Solution for destination city

---
+++ python solutions/destination_city.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        start_cities, end_cities = map(set, zip(*paths))
+        destination = (end_cities - start_cities).pop()
+        return destination
